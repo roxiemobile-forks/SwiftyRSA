e73b0ac55ef9d03f0d0fb72915eb5bd9c7ff1aab
Alexander Bragin <bragin-av@roxiemobile.com>
2022-03-22 00:20:05 +0300

    NSFPIO-46 Added support of the Swift Package Manager.

---
+++ Package.swift
@@ -0,0 +1,34 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+// Swift Package Manager — Package
+// @link https://github.com/apple/swift-package-manager/blob/main/Documentation/PackageDescription.md
+
+let package = Package(
+    name: "SwiftyRSA",
+    platforms: [
+        .iOS(.v12),
+        .macOS(.v10_12),
+        .tvOS(.v12),
+        .watchOS(.v3),
+    ],
+    products: [
+        .library(name: "SwiftyRSA", targets: ["SwiftyRSA"]),
+    ],
+    targets: [
+        .target(
+            name: "SwiftyRSA",
+            path: "Source",
+            exclude: [
+                "Info-tvOS.plist",
+                "Info.plist",
+                "SwiftyRSA.h",
+            ],
+            linkerSettings: [
+                .linkedFramework("Security"),
+            ]
+        ),
+    ],
+    swiftLanguageVersions: [.v5]
+)
